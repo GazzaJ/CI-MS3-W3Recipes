d55f4cdfde567f4b7d2e83935900398c94dc8216
GazzaJ <johnge71@gmail.com>
2021-02-23 16:25:33 +0000

    Covnert Servings select from JS to Jinja for loop

--- static/js/script.js
+++ static/js/script.js
@@ -5,13 +5,4 @@ $(document).ready(function(){
     $('.tap-target').tapTarget();
     $('.parallax').parallax();
   });
-
-  //----- Create numbers for "Servings" dropdown list 
-  var servings = document.getElementById("servings");
-  var numbers;
-
-  for (let i = 1; i <= 31; i++) {
-      numbers += "<option>" + i + "</option>";
-  }
-
-  servings.innerHTML = numbers
+  
