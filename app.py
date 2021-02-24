82994feeccecadc0b6ee1e2a3546e09be77acdee
GazzaJ <johnge71@gmail.com>
2021-02-24 13:36:43 +0000

    Render DB data onto Edit recipe form

--- app.py
+++ app.py
@@ -144,7 +144,7 @@ def manage_recipes():
 
 # ---------- Edit a Recipe ----------
 @app.route("/edit_recipe/<recipe_id>", methods=["GET", "POST"])
-def edit_recipe(recipe_id):       
+def edit_recipe(recipe_id):
     recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
     countries = mongo.db.countries.find()
     return render_template("edit_recipe.html", recipe=recipe, countries=countries)
