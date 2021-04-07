a3a589221551f17e4288ed77552b540e282c8dfa
GazzaJ <johnge71@gmail.com>
2021-04-07 10:02:19 +0000

    Change Sign-up Button colour to match website

--- app.py
+++ app.py
@@ -581,5 +581,5 @@ if __name__ == '__main__':
     app.run(
         host=os.environ.get('IP'),
         port=int(os.environ.get('PORT')),
-        debug=True)
+        debug=False)
     profanity.load_censor_words()
