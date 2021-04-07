22157c10738f5899cdf9204c2a55a00ce8bc54bb
GazzaJ <johnge71@gmail.com>
2021-04-07 16:35:43 +0000

    Fixing minor responsiveness issues with Recipe cards only visible in heroku

--- app.py
+++ app.py
@@ -581,5 +581,5 @@ if __name__ == '__main__':
     app.run(
         host=os.environ.get('IP'),
         port=int(os.environ.get('PORT')),
-        debug=False)
+        debug=True)
     profanity.load_censor_words()
