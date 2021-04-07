bf870e5bca664310cc8e2668ba74a68e6335be07
GazzaJ <johnge71@gmail.com>
2021-04-07 11:21:13 +0000

    Corrections to Script.js file based on JSHint checks

--- static/js/script.js
+++ static/js/script.js
@@ -1,11 +1,11 @@
 /*----- Materialize CSS JQuery Helpers -----*/
 $(document).ready(function(){
-    $('.sidenav').sidenav({edge: "right"});
-    $('.tooltipped').tooltip(); 
-    $('select').formSelect();    
-    $('.parallax').parallax();
-    $('.fixed-action-btn').floatingActionButton();
-    $('.modal').modal();
+    $(".sidenav").sidenav({edge: "right"});
+    $(".tooltipped").tooltip(); 
+    $("select").formSelect();    
+    $(".parallax").parallax();
+    $(".fixed-action-btn").floatingActionButton();
+    $(".modal").modal();
 });
 
 /*----- Display image when URL provided ----- 
@@ -19,74 +19,73 @@ function addImage()
 }
 
 /*---------- FAB for Recipe Edit & Delete ----------*/
-document.addEventListener('DOMContentLoaded', function() {
-var elems = document.querySelectorAll('.fixed-action-btn');
+document.addEventListener("DOMContentLoaded", function() {
+var elems = document.querySelectorAll(".fixed-action-btn");
 var instances = M.FloatingActionButton.init(elems, {
-    direction: 'left'
+    direction: "left"
 });
 });
 
 /*----- Check Status of "Subscribed" checkbox ----- 
 ---------- on the Edit Profile Page -----*/
 $(document).ready(function(){
-    if ($('#subscribed').is(':checked')) {
-        $('#subs-email').show();
+    if ($("#subscribed").is(":checked")) {
+        $("#subs-email").show();
     } else {            
-        $('#subs-email').hide();
+        $("#subs-email").hide();
     }
 });
 
 /*----- Show Textbox to enter email address if user -----
 ----- wants to subscribe, or hide textbox if not -----*/
 $(document).ready(function(){
-    $('.switch :checkbox').change(function(){        
-        if ($(this).is(':checked')) {
+    $(".switch :checkbox").change(function(){        
+        if ($(this).is(":checked")) {
             $('#subs-email').show();            
         } else {
-            $('#subs-email').hide();            
+            $("#subs-email").hide();            
         }
-    })
+    });
 });
 
 /*----- Change footer link colour on Hover -----*/
 $(document).ready(function(){
     $(".hover").hover(function(){
-        $(this).toggleClass("toggle")
-    })
-})
+        $(this).toggleClass("toggle");
+    });
+});
 
 /*----- Change signup link colour on Hover -----*/
 $(document).ready(function(){
-    $(".cta-signup").hover(function(){
-        $(this).toggleClass("toggle2")
-    })
-})
+    $(".cta-signup").hover(function() {
+        $(this).toggleClass("toggle2");
+    });
+});
 
 /*----- Highlight navbar links on Hover -----*/
 $(document).ready(function(){
     $(".navlink").hover(function(){
-        $(this).toggleClass("toggle2")
-    })
-})
+        $(this).toggleClass("toggle2");
+    });
+});
 
 /*----- Toggle sign-up link on Hover -----*/
 $(document).ready(function(){
     $(".signup-2").hover(function(){
-        $(this).toggleClass("toggle3")
-    })
-})
+        $(this).toggleClass("toggle3");
+    });
+});
 
 /*----- Toggle img-url link on Hover -----*/
 $(document).ready(function(){
     $(".img-link").hover(function(){
-        $(this).toggleClass("toggle4")
-    })
-})
+        $(this).toggleClass("toggle4");
+    });
+});
 
 /*----- Toggle recipes link on Hover -----*/
 $(document).ready(function(){
     $(".recipe-link").hover(function(){
-        $(this).toggleClass("toggle3")
-    })
-})
-
+        $(this).toggleClass("toggle3");
+    });
+});
